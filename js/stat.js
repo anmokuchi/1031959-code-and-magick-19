14c200bf4d94296b058bd84b2c5ba3bfefad6902
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-02-11 00:31:35 +0900

    Почищено по Тревису

--- js/stat.js
+++ js/stat.js
@@ -2,8 +2,6 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
 var MESSAGE_X = 120;
 var MESSAGE_Y = 40;
 var GAP = 50;
@@ -13,12 +11,12 @@ var STATS_X = 140;
 var NAMES_Y = 260;
 var NAMES_GAP = 20;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -30,7 +28,7 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
@@ -44,12 +42,12 @@ window.renderStatistics = function(ctx, names, times) {
   for (var i = 0; i < names.length; i++) {
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      } else {
+    } else {
       ctx.fillStyle = 'hsl(240, 100%, ' + Math.round(Math.random() * 100) + '%)';
-      }
-  ctx.fillRect(STATS_X + (BAR_WIDTH + GAP) * i, NAMES_Y - NAMES_GAP - ((BAR_MAX_HEIGHT * times[i]) / maxTime), BAR_WIDTH, (BAR_MAX_HEIGHT * times[i]) / maxTime);
-  ctx.fillStyle = '#000';
-  ctx.fillText(Math.round(times[i]), STATS_X + (BAR_WIDTH + GAP) * i, NAMES_Y - NAMES_GAP * 1.2 - ((BAR_MAX_HEIGHT * times[i]) / maxTime));
-  ctx.fillText(names[i], STATS_X + (BAR_WIDTH + GAP) * i, NAMES_Y);
- }
+    }
+    ctx.fillRect(STATS_X + (BAR_WIDTH + GAP) * i, NAMES_Y - NAMES_GAP - ((BAR_MAX_HEIGHT * times[i]) / maxTime), BAR_WIDTH, (BAR_MAX_HEIGHT * times[i]) / maxTime);
+    ctx.fillStyle = '#000';
+    ctx.fillText(Math.round(times[i]), STATS_X + (BAR_WIDTH + GAP) * i, NAMES_Y - NAMES_GAP * 1.2 - ((BAR_MAX_HEIGHT * times[i]) / maxTime));
+    ctx.fillText(names[i], STATS_X + (BAR_WIDTH + GAP) * i, NAMES_Y);
+  }
 };
