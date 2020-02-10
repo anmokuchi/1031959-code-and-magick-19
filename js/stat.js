1d6b71c6d19a7dde2bedf61fb53f94605409eddc
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-02-10 19:25:26 +0900

    Размер облака перенесен в константу, для отрисовки создана функция

--- js/stat.js
+++ js/stat.js
@@ -1,9 +1,14 @@
 'use strict';
 
-window.renderStatistics = function(ctx) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(100, 10, 420, 270);
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function(ctx) {
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#fff');
 };
