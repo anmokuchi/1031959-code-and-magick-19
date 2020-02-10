05b650fd8ef52c39e6fe2cb707ba409524f63523
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-02-10 19:18:14 +0900

    Добавлена полупрозрачная черная тень, смещенная на 10 пикселей вниз и вправо

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,9 @@
 'use strict';
 
 window.renderStatistics = function(ctx) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+
   ctx.fillStyle = '#fff';
   ctx.fillRect(100, 10, 420, 270);
 };
