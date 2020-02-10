68bdea93737c93913fdcec0722245cb7b6a82abc
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-02-10 20:04:26 +0900

    Отрисованы наброски столбцов для гистограммы

--- js/stat.js
+++ js/stat.js
@@ -16,4 +16,17 @@ window.renderStatistics = function(ctx) {
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Вы', 140, 260);
+  ctx.fillRect(140, 90, 40, 150);
+
+  ctx.fillText('Кекс', 230, 260);
+  ctx.fillRect(230, 90, 40, 150);
+
+  ctx.fillText('Катя', 320, 260);
+  ctx.fillRect(320, 90, 40, 150);
+
+  ctx.fillText('Игорь', 410, 260);
+  ctx.fillRect(410, 90, 40, 150);
 };
