f40f21cbaa48c123acb2a165f60dd172636603f3
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-02-15 20:38:12 +0900

    Созданы массивы с данными и похожих магах, проделаны шаги из демонстрации

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,30 @@
 'use strict';
 
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+// Показать окно настроек пользователя
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
+
+// Показать блок с похожими магами
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+// Найти элемент, куда вставлять магов
+var similarListElement = document.querySelector('.setup-similar-list');
+
+// Найти шаблон для похожих магов
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+
+// Цикл отрисовки шаблона
+for (var i = 0; i < 4; i++) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = WIZARD_NAMES[i];
+
+  similarListElement.appendChild(wizardElement);
+}
