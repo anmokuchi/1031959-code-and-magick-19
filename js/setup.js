a95ab407bb0eca99fbb37c989b8dd1a868fca20b
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-02-15 22:21:34 +0900

    Исправлены ошибки в цветах, работе функции нахождения рандомного элемента массива, добавлена функция добавления магов на страницу

--- js/setup.js
+++ js/setup.js
@@ -2,21 +2,18 @@
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb (0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_AMOUNT = 4;
 
 // Функция нахождения рандомного элемента массива
-var getRandomElement = function (elements) {
-  return Math.floor(Math.random() * elements.length);
+var getRandomArrayIndex = function (arrayName) {
+  return arrayName[Math.floor(Math.random()*arrayName.length)];
 };
 
 // Показать окно настроек пользователя
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
-
-// Показать блок с похожими магами
-document.querySelector('.setup-similar').classList.remove('hidden');
 
 // Найти элемент, куда вставлять магов
 var similarListElement = document.querySelector('.setup-similar-list');
@@ -31,9 +28,9 @@ var getWizards = function (wizardsAmount) {
   var wizards = [];
   for (var i = 0; i < wizardsAmount; i++) {
     wizards.push({
-      name: getRandomElement(WIZARD_NAMES) + ' ' + getRandomElement(WIZARD_SURNAMES),
-      coatColor: getRandomElement(COAT_COLORS) + '',
-      eyesColor: getRandomElement(EYES_COLORS) + ''
+      name: getRandomArrayIndex(WIZARD_NAMES) + ' ' + getRandomArrayIndex(WIZARD_SURNAMES),
+      coatColor: getRandomArrayIndex(COAT_COLORS),
+      eyesColor: getRandomArrayIndex(EYES_COLORS)
     });
   }
   return wizards;
@@ -42,7 +39,26 @@ var getWizards = function (wizardsAmount) {
 // Функция отрисовки магов на странице
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
+
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes');
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
 };
+
+// Функция добавления магов во фрагмент и затем на страницу
+var addWizards = function () {
+  var wizards = getWizards(WIZARDS_AMOUNT);
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
+
+// Вызов функции добавления магов
+addWizards();
+
+// Показать блок с похожими магами
+document.querySelector('.setup-similar').classList.remove('hidden');
