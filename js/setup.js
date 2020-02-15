70494dd7a2c9d5e26438e44f7fb2c2a5a12c26d4
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-02-15 21:16:04 +0900

    Добавлена функция нахождения случайного элемента массива и функция генерации параметров магов

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,12 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_AMOUNT = 4;
+
+// Функция нахождения рандомного элемента массива
+var getRandomElement = function (elements) {
+  return Math.floor(Math.random() * elements.length);
+};
 
 // Показать окно настроек пользователя
 var userDialog = document.querySelector('.setup');
@@ -19,6 +25,19 @@ var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
+
+// Собрать параметры магов
+var getWizards = function (wizardsAmount) {
+  var wizards = [];
+  for (var i = 0; i < wizardsAmount; i++) {
+    wizards.push({
+      name: getRandomElement(WIZARD_NAMES) + ' ' + getRandomElement(WIZARD_SURNAMES),
+      coatColor: getRandomElement(COAT_COLORS) + '',
+      eyesColor: getRandomElement(EYES_COLORS) + ''
+    });
+  }
+  return wizards;
+};
 
 // Цикл отрисовки шаблона
 for (var i = 0; i < 4; i++) {
