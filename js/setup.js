313b0b5e7fc98fbd015b603fe0d8e33e7854513b
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-02-28 23:32:34 +0900

    Добавлены обработчики по клику, изменяющие цвета плаща, глаз и файербола мага по порядку в массивах цветов, произведен рефакторинг кода

--- js/setup.js
+++ js/setup.js
@@ -3,12 +3,12 @@
 var ESC_KEY = 'Escape';
 var ENTER_KEY = 'Enter';
 
+var WIZARDS_AMOUNT = 4;
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb (0, 0, 0)'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-var WIZARDS_AMOUNT = 4;
 
 // Переменные с окном настроек пользователя
 var setup = document.querySelector('.setup');
@@ -17,7 +17,7 @@ var setupClose = setup.querySelector('.setup-close');
 var userNameInput = setup.querySelector('.setup-user-name');
 
 // Обработчик закрытия окна по нажатию на Escape
-var onPopupEscPress = function (evt) {
+var popupEscPressHandler = function (evt) {
   if (evt.target === userNameInput) {
     evt.stopPropagation();
   } else if (evt.key === ESC_KEY) {
@@ -28,13 +28,13 @@ var onPopupEscPress = function (evt) {
 // Функция открытия окна
 var openPopup = function () {
   setup.classList.remove('hidden');
-  document.addEventListener('keydown', onPopupEscPress);
+  document.addEventListener('keydown', popupEscPressHandler);
 };
 
 // Функция закрытия окна
 var closePopup = function () {
   setup.classList.add('hidden');
-  document.removeEventListener('keydown', onPopupEscPress);
+  document.removeEventListener('keydown', popupEscPressHandler);
 };
 
 // Обработчик открытия окна настроек по клику (закрывается при нажатии Escape)
@@ -60,6 +60,62 @@ setupClose.addEventListener('keydown', function (evt) {
     closePopup();
   }
 });
+
+// Переменные с настройками цвета персонажа
+var setupPlayer = setup.querySelector('.setup-player');
+var wizardCoat = setupPlayer.querySelector('.wizard-coat');
+var wizardCoatInput = setupPlayer.querySelector('input[name="coat-color"]');
+var wizardEyes = setupPlayer.querySelector('.wizard-eyes');
+var wizardEyesInput = setupPlayer.querySelector('input[name="eyes-color"]');
+var wizardFireball = setup.querySelector('.setup-fireball-wrap');
+var wizardFireballInput = setupPlayer.querySelector('input[name="fireball-color"]');
+
+// Переменные с начальным индексом
+var coatColorIndex = 0;
+var eyesColorIndex = 0;
+var fireballColorIndex = 0;
+
+// Функция получения следующего индекса массива
+var getNextIndex = function (index, elements) {
+  return (index + 1) % elements.length;
+};
+
+// Функция изменения цвета заливки
+var changeColor = function (element, elementInput, colorIndex, colors) {
+  var newColor = colors[colorIndex];
+  element.style.fill = newColor;
+  elementInput.value = newColor;
+};
+
+// Функция изменения цвета фона
+var changeBackgroundColor = function (element, elementInput, colorIndex, colors) {
+  var newColor = colors[colorIndex];
+  element.style.background = newColor;
+  elementInput.value = newColor;
+};
+
+// Обработчик изменения цвета плаща мага
+var wizardCoatClickHandler = function () {
+  coatColorIndex = getNextIndex(coatColorIndex, COAT_COLORS);
+  changeColor(wizardCoat, wizardCoatInput, coatColorIndex, COAT_COLORS);
+};
+
+// Обработчик изменения цвета глаз мага
+var wizardEyesClickHandler = function () {
+  eyesColorIndex = getNextIndex(eyesColorIndex, EYES_COLORS);
+  changeColor(wizardEyes, wizardEyesInput, eyesColorIndex, EYES_COLORS);
+};
+
+// Обработчик изменения цвета файербола
+var wizardFireballClickHandler = function () {
+  fireballColorIndex = getNextIndex(fireballColorIndex, FIREBALL_COLORS);
+  changeBackgroundColor(wizardFireball, wizardFireballInput, fireballColorIndex, FIREBALL_COLORS);
+};
+
+// Применение обработчиков по клику
+wizardCoat.addEventListener('click', wizardCoatClickHandler);
+wizardEyes.addEventListener('click', wizardEyesClickHandler);
+wizardFireball.addEventListener('click', wizardFireballClickHandler);
 
 // Функция нахождения рандомного элемента массива
 var getRandomArrayElement = function (elements) {
