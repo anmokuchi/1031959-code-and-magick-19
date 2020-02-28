8bc2f4758635f5277410df5a3a9fb52f58280f07
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-02-28 17:53:05 +0900

    Добавлена проверка условия: если фокус находится на форме ввода имени, то окно закрываться не должно

--- js/setup.js
+++ js/setup.js
@@ -7,16 +7,20 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb (0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var WIZARDS_AMOUNT = 4;
 
 // Переменные с окном настроек пользователя
 var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = setup.querySelector('.setup-close');
+var userNameInput = setup.querySelector('.setup-user-name');
 
 // Обработчик закрытия окна по нажатию на Escape
 var onPopupEscPress = function (evt) {
-  if (evt.key === ESC_KEY) {
+  if (evt.target === userNameInput) {
+    evt.stopPropagation();
+  } else if (evt.key === ESC_KEY) {
     closePopup();
   }
 };
