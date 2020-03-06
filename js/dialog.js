ba82968f3e1d7e446024b7ccc7bd30741777e449
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-03-06 22:39:09 +0900

    Добавлен и подключен файл скрипта dialog

---
+++ js/dialog.js
@@ -0,0 +1,46 @@
+'use strict'
+
+(function () {
+  // Переменные с окном настроек пользователя
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var userNameInput = setup.querySelector('.setup-user-name');
+
+// Обработчик закрытия окна по нажатию на Escape
+var popupEscPressHandler = function(evt) {
+  window.util.isEscEventTargetExclude(evt, userNameInput, closePopup);
+};
+
+// Функция открытия окна
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', popupEscPressHandler);
+};
+
+// Функция закрытия окна
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', popupEscPressHandler);
+};
+
+// Обработчик открытия окна настроек по клику (закрывается при нажатии Escape)
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+// Обработчик открытия окна настроек по нажатию на Enter
+setupOpen.addEventListener('keydown', function (evt) {
+  window.util.isEnterEvent(evt, openPopup);
+});
+
+// Обработчик закрытия окна настроек по клику
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+// Обработчик закрытия окна настроек по нажатию на Enter
+setupClose.addEventListener('keydown', function (evt) {
+  window.util.isEnterEvent(evt, closePopup);
+});
+})();
--- js/setup.js
+++ js/setup.js
@@ -7,49 +7,6 @@ var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
-// Переменные с окном настроек пользователя
-var setup = document.querySelector('.setup');
-var setupOpen = document.querySelector('.setup-open');
-var setupClose = setup.querySelector('.setup-close');
-var userNameInput = setup.querySelector('.setup-user-name');
-
-// Обработчик закрытия окна по нажатию на Escape
-var popupEscPressHandler = function(evt) {
-  window.util.isEscEventTargetExclude(evt, userNameInput, closePopup);
-};
-
-// Функция открытия окна
-var openPopup = function () {
-  setup.classList.remove('hidden');
-  document.addEventListener('keydown', popupEscPressHandler);
-};
-
-// Функция закрытия окна
-var closePopup = function () {
-  setup.classList.add('hidden');
-  document.removeEventListener('keydown', popupEscPressHandler);
-};
-
-// Обработчик открытия окна настроек по клику (закрывается при нажатии Escape)
-setupOpen.addEventListener('click', function () {
-  openPopup();
-});
-
-// Обработчик открытия окна настроек по нажатию на Enter
-setupOpen.addEventListener('keydown', function (evt) {
-  window.util.isEnterEvent(evt, openPopup);
-});
-
-// Обработчик закрытия окна настроек по клику
-setupClose.addEventListener('click', function () {
-  closePopup();
-});
-
-// Обработчик закрытия окна настроек по нажатию на Enter
-setupClose.addEventListener('keydown', function (evt) {
-  window.util.isEnterEvent(evt, closePopup);
-});
-
 // Переменные с настройками цвета персонажа
 var setupPlayer = setup.querySelector('.setup-player');
 var wizardCoat = setupPlayer.querySelector('.wizard-coat');
