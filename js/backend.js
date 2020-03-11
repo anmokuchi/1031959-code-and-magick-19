4d5b61769827013d89262274b58c06cb23778830
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-03-11 18:30:04 +0900

    Добавлена функция backend.load

--- js/backend.js
+++ js/backend.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (function () {
+  var LOAD_URL = 'https://js.dump.academy/code-and-magick/data';
   var SAVE_URL = 'https://js.dump.academy/code-and-magick';
 
   var statusCode = {
@@ -9,6 +10,35 @@
 
   var TIMEOUT_IN_MS = 10000; // 10 s;
 
+  // Функция загрузки данных волшебников с сервера
+  var load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.open('GET', LOAD_URL);
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === statusCode.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.send();
+  };
+
+  // Функция загрузки данных о персонаже на сервер
   var save = function (data, onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
