7c88ba191a54d3123dcb262025ad090939314b6c
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-03-11 19:05:12 +0900

    Добавлен внешний вид сообщения об ошибке

--- js/setup.js
+++ js/setup.js
@@ -66,11 +66,24 @@
     });
   });
 
+  // Внешний вид сообщения об ошибке
+  var onErrorMessageStyle = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
   // Обработчик отправки данных
   form.addEventListener('submit', function (evt) {
     window.backend.save(new FormData(form), function () {
       setup.classList.add('hidden');
-    }, /* тут стиль окна с ошибкой */);
+    }, onErrorMessageStyle);
     evt.preventDefault();
   });
 })();
--- js/similar-wizards.js
+++ js/similar-wizards.js
@@ -23,7 +23,7 @@
   };
 
   // Функция добавления магов на страницу
-  window.backend.load(function (wizards) {
+  var onSuccess = function (wizards) {
     var fragment = document.createDocumentFragment();
 
     for (var i = 0; i < WIZARDS_AMOUNT; i++) {
@@ -33,5 +33,20 @@
 
     // Показать блок с похожими магами
     document.querySelector('.setup-similar').classList.remove('hidden');
-  });
+  };
+
+  // Внешний вид сообщения об ошибке
+  var onErrorMessageStyle = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(onSuccess, onErrorMessageStyle);
 })();
